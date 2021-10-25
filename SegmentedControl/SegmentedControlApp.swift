ab0fe96d03e9b7ad77facb8162da66b330dab928
farkhod <986532qw@gmail.com>
2021-10-25 15:36:22 +0300

    Initial Commit

---
+++ SegmentedControl/SegmentedControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControlApp.swift
+//  SegmentedControl
+//
+//  Created by Farkhod on 25.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
